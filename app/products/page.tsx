129395481f8179fe27f5feeda506081b384a969b
Colton923 <110353948+Colton923@users.noreply.github.com>
2023-04-15 13:50:40 -0400

    .

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,82 +9,85 @@ export default function Products() {
         have a variety of crystals, essential oils, and other items to help you on
         your journey.
       </p>
-      <h3 className={styles.missionHeader}>Animal Totems</h3>
-      <p className={styles.missionText}>
-        Readings to learn your nine totem animals, and understand their influence in
-        your life.
-      </p>
-      <h3 className={styles.missionHeader}>Ear Candling</h3>
-      <p className={styles.missionText}>
-        An ancient art form used around the world to remove ear wax and candida
-        buildup.
-      </p>
-      <h3 className={styles.missionHeader}>Holistic Counseling</h3>
-      <p className={styles.missionText}>
-        Integrate a healthy sense of Self in relationship with body, mind, emotion
-        and spirit, which in turn creates a healthy sense of Self in all
-        relationships. Create wholeness and empowerment by clarifying, moving toward,
-        and/or achieving your goals. Reveal and release what gets in your way.
-        Embrace all parts of yourself, turning the inner critic into your ally.
-        Reconnect body, mind and spirit.
-      </p>
-      <h3 className={styles.missionHeader}>Ionic Foot Spa</h3>
-      <p className={styles.missionText}>
-        A foot bath with electrically charged ions to pull neutralized particles out
-        of the body.
-      </p>
-      <h3 className={styles.missionHeader}>Massage Therapy</h3>
-      <p className={styles.missionText}>
-        Promotes relaxation, improves circulation and range of motion. Relieves
-        muscle tension.
-      </p>
-      <h3 className={styles.missionHeader}>Meditation</h3>
-      <p className={styles.missionText}>
-        An approach to bring greater peace, relaxation, awareness and overall Health.
-      </p>
-      <h3 className={styles.missionHeader}>Numerology</h3>
-      <p className={styles.missionText}>
-        The study of the spiritual qualities of numbers and letters giving you
-        detailed insight into your personality, your direction in life
-      </p>
-      <h3 className={styles.missionHeader}>Reiki</h3>
-      <p className={styles.missionText}>
-        A Japanese technique for stress reduction and relaxation that also promotes
-        healing. Reiki is administered by “laying on hands” and is based on the idea
-        that an unseen “life force energy” flows through us and is what causes us to
-        be alive. If one’s “life force energy” is low, then we are more likely to get
-        sick or feel stress, and if it is high, we are more capable of being happy
-        and healthy. Reiki treats the whole person including body, emotions, mind and
-        spirit creating many beneficial effects that include relaxation and feelings
-        of peace, security and wellbeing.
-      </p>
-      <h3 className={styles.missionHeader}>Sound Healing</h3>
-      <p className={styles.missionText}>
-        A variety of voice activated tones and sounds directed to move physical and
-        emotional blocks.
-      </p>
-      <h3 className={styles.missionHeader}>Trager® Approach</h3>
-      <p className={styles.missionText}>
-        Relaxes, soothes and unwinds the body, mind and spirit. It is a gentle,
-        non-invasive approach to healing that uses gentle rocking and stretching to
-        release tension and stress. It is a unique method of working with the body’s
-        natural ability to heal itself. It is a non-manipulative, non-invasive
-        approach to healing that uses gentle rocking and stretching to release
-        tension and stress. It is a unique method of working with the body’s natural
-        ability to heal itself. It is a non-manipulative, non-invasive approach to
-        healing that uses gentle rocking and stretching to release tension and
-        stress. It is a unique method of working with the body’s natural ability to
-        heal itself.
-      </p>
-      <h3 className={styles.missionHeader}>Reflexology</h3>
-      <p className={styles.missionText}>
-        Is a science that deals with the principle that there are reflex areas in the
-        feet and hands that correspond to all of the glands, organs and parts of the
-        body. Reflexology is a unique method of using the thumb and fingers on these
-        reflex areas. Reflexology includes, but is not limited to: - Relieve stress
-        and tension; - Improve blood supply and promote the unblocking of nerve
-        impulses; - Help your body achieve natural balance{' '}
-      </p>
+      <div className={styles.products}>
+        <h3 className={styles.missionHeader}>Animal Totems</h3>
+        <p className={styles.missionText}>
+          Readings to learn your nine totem animals, and understand their influence
+          in your life.
+        </p>
+        <h3 className={styles.missionHeader}>Ear Candling</h3>
+        <p className={styles.missionText}>
+          An ancient art form used around the world to remove ear wax and candida
+          buildup.
+        </p>
+        <h3 className={styles.missionHeader}>Holistic Counseling</h3>
+        <p className={styles.missionText}>
+          Integrate a healthy sense of Self in relationship with body, mind, emotion
+          and spirit, which in turn creates a healthy sense of Self in all
+          relationships. Create wholeness and empowerment by clarifying, moving
+          toward, and/or achieving your goals. Reveal and release what gets in your
+          way. Embrace all parts of yourself, turning the inner critic into your
+          ally. Reconnect body, mind and spirit.
+        </p>
+        <h3 className={styles.missionHeader}>Ionic Foot Spa</h3>
+        <p className={styles.missionText}>
+          A foot bath with electrically charged ions to pull neutralized particles
+          out of the body.
+        </p>
+        <h3 className={styles.missionHeader}>Massage Therapy</h3>
+        <p className={styles.missionText}>
+          Promotes relaxation, improves circulation and range of motion. Relieves
+          muscle tension.
+        </p>
+        <h3 className={styles.missionHeader}>Meditation</h3>
+        <p className={styles.missionText}>
+          An approach to bring greater peace, relaxation, awareness and overall
+          Health.
+        </p>
+        <h3 className={styles.missionHeader}>Numerology</h3>
+        <p className={styles.missionText}>
+          The study of the spiritual qualities of numbers and letters giving you
+          detailed insight into your personality, your direction in life
+        </p>
+        <h3 className={styles.missionHeader}>Reiki</h3>
+        <p className={styles.missionText}>
+          A Japanese technique for stress reduction and relaxation that also promotes
+          healing. Reiki is administered by “laying on hands” and is based on the
+          idea that an unseen “life force energy” flows through us and is what causes
+          us to be alive. If one’s “life force energy” is low, then we are more
+          likely to get sick or feel stress, and if it is high, we are more capable
+          of being happy and healthy. Reiki treats the whole person including body,
+          emotions, mind and spirit creating many beneficial effects that include
+          relaxation and feelings of peace, security and wellbeing.
+        </p>
+        <h3 className={styles.missionHeader}>Sound Healing</h3>
+        <p className={styles.missionText}>
+          A variety of voice activated tones and sounds directed to move physical and
+          emotional blocks.
+        </p>
+        <h3 className={styles.missionHeader}>Trager® Approach</h3>
+        <p className={styles.missionText}>
+          Relaxes, soothes and unwinds the body, mind and spirit. It is a gentle,
+          non-invasive approach to healing that uses gentle rocking and stretching to
+          release tension and stress. It is a unique method of working with the
+          body’s natural ability to heal itself. It is a non-manipulative,
+          non-invasive approach to healing that uses gentle rocking and stretching to
+          release tension and stress. It is a unique method of working with the
+          body’s natural ability to heal itself. It is a non-manipulative,
+          non-invasive approach to healing that uses gentle rocking and stretching to
+          release tension and stress. It is a unique method of working with the
+          body’s natural ability to heal itself.
+        </p>
+        <h3 className={styles.missionHeader}>Reflexology</h3>
+        <p className={styles.missionText}>
+          Is a science that deals with the principle that there are reflex areas in
+          the feet and hands that correspond to all of the glands, organs and parts
+          of the body. Reflexology is a unique method of using the thumb and fingers
+          on these reflex areas. Reflexology includes, but is not limited to: -
+          Relieve stress and tension; - Improve blood supply and promote the
+          unblocking of nerve impulses; - Help your body achieve natural balance{' '}
+        </p>
+      </div>
     </div>
   )
 }
